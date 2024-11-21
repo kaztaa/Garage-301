8517f4d6bdb2ea89b7ddc460ff92eb5960e38d3e
dzta <dzta@protonmail.com>
2024-11-21 15:08:23 +0100

    User seed data

---
+++ Garage2/Extentions/ApplicationBuilderExtentions.cs
@@ -0,0 +1,28 @@
+﻿using Garage301.Data;
+using Gym.Data;
+
+namespace Garage301.Extentions
+{
+    public static class ApplicationBuilderExtentions
+    {
+        public static async Task<IApplicationBuilder> SeedDataAsync(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<Garage301Context>();
+
+                try
+                {
+                    await SeedData.Init(context, services);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                return app;
+            }
+        }
+    }
+}
