368cbe41fda827d6c306ea05235bcc35936768f7
Geekzz <jjohan91@hotmail.com>
2024-11-23 13:46:14 +0100

    Added userClaim to display name

--- Garage2/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Garage2/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
@@ -148,6 +149,7 @@ namespace Garage301.Areas.Identity.Pages.Account
                 // Create the user with the provided password
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 await _userManager.AddToRoleAsync(user, "Member");
+                await _userManager.AddClaimAsync(user, new Claim("FullName", user.FullName));
 
                 if (result.Succeeded)
                 {
